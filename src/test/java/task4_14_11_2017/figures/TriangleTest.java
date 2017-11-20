57a9eff2899a53d516729ea75cd56916d4791f47
Sbitneva <masha.sbitneva@gmail.com>
2017-11-21 00:39:31 +0200

    hw4: triangle tests minor fix

--- src/test/java/task4_14_11_2017/figures/TriangleTest.java
+++ src/test/java/task4_14_11_2017/figures/TriangleTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import task4_14_11_2017.factories.TriangleFactory;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TriangleTest {
@@ -11,15 +12,16 @@ public class TriangleTest {
     private Triangle triangle2;
 
     @Before public void createTriangle(){
-        triangle1 = TriangleFactory.create();
+        triangle1 = new Triangle( new Point(1, 1), new Point(1, 1), new Point(2, 1));
         triangle2 = TriangleFactory.create();
     }
     @Test public void testValidation(){
-        assertTrue("Triangle1 apexes are wrong", triangle1.isValid());
+        assertFalse("Triangle1 apexes are wrong", triangle1.isValid());
         assertTrue("Triangle2 apexes are wrong", triangle2.isValid());
     }
 
     @Test public void testSquare(){
+        triangle1 = TriangleFactory.create();
         assertTrue("Triangle1 square wrong", triangle1.getSquare() > 0);
         assertTrue("Triangle2 square wrong", triangle2.getSquare() > 0);
     }
