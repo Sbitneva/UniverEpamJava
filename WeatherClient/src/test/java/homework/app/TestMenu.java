99b43e71d14015caac8c796e62904ccf4148dbc4
Sbitneva <masha.sbitneva@gmail.com>
2017-11-24 01:39:48 +0200

    hw5: GUI dependent tests ignored

--- WeatherClient/src/test/java/homework/app/TestMenu.java
+++ WeatherClient/src/test/java/homework/app/TestMenu.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.junit.Ignore;
 import view.Menu;
 
 import static org.junit.Assert.assertTrue;
@@ -6,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class TestMenu {
     Menu menu;
 
+    @Ignore("Can't test GUI on CI job")
     @Test public void isMenuElementsVisible(){
         menu = new Menu();
         assertTrue("JFrame visibility problem", menu.getJf().isVisible());
